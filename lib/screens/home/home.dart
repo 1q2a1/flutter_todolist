ca73dca0fd9a3e2d3647aa1c09596ae1228abba6
mit <72991363+1q2a1@users.noreply.github.com>
2024-06-08 15:15:31 +0800

    Added Listview widget to display todoCards on home page

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,15 +8,33 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, String>> todoItems = [
+      {"task": "Task1", "dueDate": "01/01/22"},
+      {"task": "Task2", "dueDate": "01/01/23"},
+      {"task": "Task3", "dueDate": "02/01/23"},
+      {"task": "Task4", "dueDate": "03/01/23"},
+      {"task": "Task5", "dueDate": "04/01/23"},
+      {"task": "Task5", "dueDate": "04/01/23"},
+      {"task": "Task5", "dueDate": "04/01/23"},
+      {"task": "Task5", "dueDate": "04/01/23"},
+      {"task": "Task5", "dueDate": "04/01/23"},
+      {"task": "Task5", "dueDate": "04/01/23"},
+    ];
+
     return Scaffold(
-      appBar: AppBar(title: const Text("To Do List")),
-      body: ListView(children: const [
-        TodoCard(
-          task: "Task1",
-          dueDate: "01/01/22",
-        ),
-        TodoCard(task: "Task2", dueDate: "01/01/23")
-      ]),
-    );
+        appBar: AppBar(title: const Text("To Do List")),
+        body: todoItems.isNotEmpty
+            ? ListView.builder(
+                itemCount: todoItems.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return TodoCard(
+                      task: todoItems[index]["task"] ?? "No Task Found",
+                      dueDate: todoItems[index]["dueDate"] ?? "No Date Found");
+                })
+            : const Center(
+                child: Text(
+                  "Nothing To Do",
+                ),
+              ));
   }
 }
--- lib/screens/home/todo_card.dart
+++ lib/screens/home/todo_card.dart
@@ -13,30 +13,28 @@ class TodoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsetsDirectional.all(20),
-        child: Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ListTile(
-                leading: const Icon(Icons.assignment),
-                title: Text(task),
-                subtitle: Text("Due: $dueDate"),
-                trailing: Row(mainAxisSize: MainAxisSize.min, children: [
-                  ElevatedButton(
-                    child: const Icon(Icons.check),
-                    onPressed: () {},
-                  ),
-                  const SizedBox(width: 10),
-                  ElevatedButton(
-                    child: const Icon(Icons.close),
-                    onPressed: () {},
-                  )
-                ]),
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            leading: const Icon(Icons.assignment),
+            title: Text(task),
+            subtitle: Text("Due: $dueDate"),
+            trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+              ElevatedButton(
+                child: const Icon(Icons.check),
+                onPressed: () {},
               ),
-            ],
+              const SizedBox(width: 10),
+              ElevatedButton(
+                child: const Icon(Icons.close),
+                onPressed: () {},
+              )
+            ]),
           ),
-        ));
+        ],
+      ),
+    );
   }
 }
